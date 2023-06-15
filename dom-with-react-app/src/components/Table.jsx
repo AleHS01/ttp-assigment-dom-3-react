3ba0d45e902cf79173156c29097bc0670b31d8ff
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-06-15 16:11:21 -0400

    creating components files

---
+++ dom-with-react-app/src/components/Table.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Table extends Component{
+    render(){
+        return (
+            <div>
+                <table>
+                    <tr>
+                        <td>1</td>
+                    </tr>
+                </table>
+            </div>
+        );
+    }
+}
